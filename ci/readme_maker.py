54c9a0abb28cd5740220de5b16b68c070e5040c9
mrroot501 <hungnguyendinh.501@gmail.com>
2024-08-23 18:14:43 +0700

    update ci

--- ci/readme_maker.py
+++ ci/readme_maker.py
@@ -3,27 +3,20 @@ from typing import List
 import sys
 
 
-README_TEMPLATE = """DevOps handbook
-===
-
-**Table of contents**
-
-{content}
-
-"""
-
 ITEM_TEMPLATE = "- [{name}]({path})\n"
 
 
 def generate_readme_content(dirs: List[str]):
     """Generate content for structure of this project for README file"""
-    content = ""
-    for path in dirs:
-        if path == "./README.md":
-            continue
-        elements = path.split("/")
-        content += ITEM_TEMPLATE.format(name="-".join(elements[2:-1]), path="/".join(elements))
-    return README_TEMPLATE.format(content=content)
+    with open(file="./ci/template.md", mode="r", encoding="utf-8") as template_file:
+        readme_template = template_file.read()
+        content = ""
+        for path in dirs:
+            if path == "./README.md":
+                continue
+            elements = path.split("/")
+            content += ITEM_TEMPLATE.format(name="-".join(elements[2:-1]), path="/".join(elements))
+        return readme_template.format(content=content)
 
 
 if __name__ == "__main__":
